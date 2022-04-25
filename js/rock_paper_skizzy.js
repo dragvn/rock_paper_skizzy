bdadf667873ef8eeb5249cf89925a593821513e3
dragvn <liminalinquiries@gmail.com>
2022-04-25 09:00:45 -0400

    setup external js file started computerPlay function

---
+++ js/rock_paper_skizzy.js
@@ -0,0 +1,17 @@
+const playerSelection = prompt('Rock, Paper, or Skizzy?')
+const computerSelection = computerPlay();
+
+function computerPlay(max) {
+  randomInt = Math.floor(Math.random() * max);
+}
+
+
+
+
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+
+console.log(computerPlay(3))
