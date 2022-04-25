655736e774aac8d0148e44b33af7a89a2ee47e23
dragvn <liminalinquiries@gmail.com>
2022-04-25 10:23:12 -0400

    got computerPlay function working

--- js/rock_paper_skizzy.js
+++ js/rock_paper_skizzy.js
@@ -2,16 +2,14 @@ const playerSelection = prompt('Rock, Paper, or Skizzy?')
 const computerSelection = computerPlay();
 
 function computerPlay(max) {
-  randomInt = Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 
 
 
 
 
-function playRound(playerSelection, computerSelection) {
-
-}
 
 
-console.log(computerPlay(3))
+
+console.log(computerPlay(3));
