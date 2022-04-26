a162caa07cc9c5aaa404505f7f88df7fc7fa4613
dragvn <liminalinquiries@gmail.com>
2022-04-26 01:14:57 -0400

    round function works alerts work, long lines of code though

--- js/rock_paper_skizzy.js
+++ js/rock_paper_skizzy.js
@@ -1,24 +1,34 @@
-const playerSelection = prompt('Rock, Paper, or Skizzy?')
-let computerSelection;
-let winner;
-
+//array
 var arr = ['rock', 'paper', 'skizzy'];
+//VARIABLES
+const playerSelection = prompt('Rock, Paper, or Skizzy?')
+const computerSelection = computerPlay();
+const win = `You win! ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)} beats ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)}`
+const lose = `You lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() +playerSelection.slice(1)}`
 
+//get comps choice from array arr at random
 function computerPlay() {
-  computerSelection = arr[Math.floor(Math.random() * arr.length)];
+  return arr[Math.floor(Math.random() * arr.length)];
 }
 
 function round(playerSelection, computerSelection) {
   if (playerSelection == 'rock' && computerSelection == 'skizzy'){
-    alert('You win!');
+    alert(win);
   }
-  else if (playerSelection < computerSelection) {
-    alert('You lose!');
+  else if (playerSelection == 'paper' && computerSelection == 'rock') {
+    alert(win);
   }
-  else if (playerSelection > computerSelection)
-    alert('It\'s a tie!!!');
+  else if (playerSelection == 'skizzy' && computerSelection == 'paper') {
+    alert(win);
+  }
+  else if (playerSelection == computerSelection) {
+    alert('Tie');
+  }
+  else {
+    alert(lose)
+  }
 }
 
-computerPlay();
-round();
 console.log(computerSelection);
+computerPlay();
+round(playerSelection, computerSelection);
