d8efb54904f20d4ce6ce0ec4fd0af6492ae5efdb
dragvn <liminalinquiries@gmail.com>
2022-04-25 23:06:37 -0400

    began working on round function. if statement bugged (logic not syntax) use other conditional statement

--- js/rock_paper_skizzy.js
+++ js/rock_paper_skizzy.js
@@ -1,5 +1,6 @@
 const playerSelection = prompt('Rock, Paper, or Skizzy?')
 let computerSelection;
+let winner;
 
 var arr = ['rock', 'paper', 'skizzy'];
 
@@ -7,5 +8,17 @@ function computerPlay() {
   computerSelection = arr[Math.floor(Math.random() * arr.length)];
 }
 
+function round(playerSelection, computerSelection) {
+  if (playerSelection == 'rock' && computerSelection == 'skizzy'){
+    alert('You win!');
+  }
+  else if (playerSelection < computerSelection) {
+    alert('You lose!');
+  }
+  else if (playerSelection > computerSelection)
+    alert('It\'s a tie!!!');
+}
+
 computerPlay();
+round();
 console.log(computerSelection);
